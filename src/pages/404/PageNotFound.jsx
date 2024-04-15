954b88704074cf2cf25b115d78c7d28ff589b28c
MutazaMadadi <murtazamadadi12@gmail.com>
2024-04-15 11:32:12 +0330

    changin applyed

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,9 +1,18 @@
-import './PageNotFound.scss'
 
-function PageNotFound() {
-  return (
-    <div>PageNotFound</div>
-  )
-}
 
-export default PageNotFound
+import "./PageNotFound.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
